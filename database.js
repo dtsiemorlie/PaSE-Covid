e22307189c0764bdf5c7dabc8a46c0effc27ee5a
dtsiemorlie <tsiemorlie@gmail.com>
2020-09-02 11:36:46 +0100

    first commit

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //local uri="mongodb://localhost:27017/PaseCovid"
 const uri =
-  "mongodb+srv://Oscar:bWOxr0CAmfm2Lznj@cluster0.pkhui.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  "mongodb+srv://<username>:<password>@cluster0.pkhui.mongodb.net/<dbname>?retryWrites=true&w=majority";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
